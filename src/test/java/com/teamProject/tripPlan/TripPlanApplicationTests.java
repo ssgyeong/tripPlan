0c3ac6bfcfe1026579da795e9cd7f455f636dce6
dev_Jang <dev.hyelim.it@gmail.com>
2024-10-22 10:22:34 +0900

    1st commit test

---
+++ src/test/java/com/teamProject/tripPlan/TripPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamProject.tripPlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
