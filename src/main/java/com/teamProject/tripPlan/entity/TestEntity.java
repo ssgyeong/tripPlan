f7eafa64f4604d32205b37aea1304426a95650e4
dev_Jang <dev.hyelim.it@gmail.com>
2024-10-22 13:02:42 +0900

    postgresql 연결 설정 test 페이지 연결

---
+++ src/main/java/com/teamProject/tripPlan/entity/TestEntity.java
@@ -0,0 +1,10 @@
+package com.teamProject.tripPlan.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class TestEntity {
+    @Id
+    private String test;
+}
