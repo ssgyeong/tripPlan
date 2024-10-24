43ebaf21789c8f8abc243171df6820ee1d4b455e
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-10-24 11:10:17 +0900

    MainController 경로 수정
    다른 페이지랑 겹쳐서 메인은 경로 아예 뺌

--- src/main/java/com/teamProject/tripPlan/controller/MainController.java
+++ src/main/java/com/teamProject/tripPlan/controller/MainController.java
@@ -8,29 +8,4 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-
-    @GetMapping("/")
-    public String Test() {
-        return "main";
-    }
-
-    @GetMapping("/community")
-    public String communityTest() {
-        return "community";
-    }
-
-    @PostMapping("/community")
-    public String showCommunity(
-            @RequestParam(required = false) String keyword,
-            @RequestParam(required = false) String area,
-            @RequestParam String departureDate,
-            @RequestParam String arrivalDate,
-            Model model) {
-        model.addAttribute("keyword", keyword);
-        model.addAttribute("area", area);
-        model.addAttribute("departureDate", departureDate);
-        model.addAttribute("arrivalDate", arrivalDate);
-
-        return "community";
-    }
 }
