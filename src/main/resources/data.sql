4a9f3abc85b9486f432e64a2081be3a6c9ec0fc1
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-10-24 18:12:31 +0900

    data 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,10 @@
-INSERT INTO keyword (keyword) VALUES ('호캉스');
+INSERT INTO keyword (keyword) VALUES ('숙박');
 INSERT INTO keyword (keyword) VALUES ('맛집');
+INSERT INTO keyword (keyword) VALUES ('자연');
+INSERT INTO keyword (keyword) VALUES ('역사');
+INSERT INTO keyword (keyword) VALUES ('체험');
+INSERT INTO keyword (keyword) VALUES ('쇼핑');
+INSERT INTO keyword (keyword) VALUES ('문화');
 
 INSERT INTO area (area) VALUES ('서울특별시');
 INSERT INTO area (area) VALUES ('부산광역시');
@@ -18,3 +23,11 @@ INSERT INTO area (area) VALUES ('전라남도');
 INSERT INTO area (area) VALUES ('경상북도');
 INSERT INTO area (area) VALUES ('경상남도');
 INSERT INTO area (area) VALUES ('제주특별자치도');
+
+INSERT INTO users (user_name, user_nickname, user_email, user_id, user_password) VALUES ('frog', 'froggy', 'frog@naver.com', 'froggg', 'frogfrog');
+INSERT INTO users (user_name, user_nickname, user_email, user_id, user_password) VALUES ('ant', 'antty', 'ant@naver.com', 'anttt', 'antant');
+
+INSERT INTO post (likes, post_date, user_no, post_title, post_content) VALUES (100, '2024-07-23', 1, '부산 조아', '부산 개조음');
+INSERT INTO post (likes, post_date, user_no, post_title, post_content) VALUES (159, '2024-08-11', 2, '여수 조아', '여수 개조음');
+
+INSERT INTO travel (end_date, start_date, user_id, keyword, place) VALUES ('2024-07-21', '2024-07-19', 1, '호캉스', '부산')
