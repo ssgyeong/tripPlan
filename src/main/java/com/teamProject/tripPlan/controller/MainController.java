b02b89988411da8a3217a9df8a93bc57938b11c8
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-10-28 12:41:40 +0900

    MainController

--- src/main/java/com/teamProject/tripPlan/controller/MainController.java
+++ src/main/java/com/teamProject/tripPlan/controller/MainController.java
@@ -15,17 +15,11 @@ public class MainController {
         return "main";
     }
 
-    @PostMapping("/result")
+    @PostMapping("/main")
     public String submit(@ModelAttribute MainDTO mainDTO, Model model) {
-        // 받은 데이터 출력
-//        System.out.println("Keyword: " + mainDTO.getKeyword());
-//        System.out.println("Area: " + mainDTO.getArea());
-//        System.out.println("Travel ID: " + mainDTO.getTravelId());
-//        System.out.println("Departure Date: " + mainDTO.getDepartureDate());
-//        System.out.println("Arrival Date: " + mainDTO.getArrivalDate());
-
         model.addAttribute("searchResult", mainDTO);
+        
         // 결과 페이지로 리다이렉트 또는 이동
-        return "result"; // result.html 페이지로 이동
+        return "main";
     }
 }
