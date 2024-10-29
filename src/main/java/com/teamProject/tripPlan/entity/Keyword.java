517270f2daa20214d757d0f380a5e92cc34ea237
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-10-29 14:47:16 +0900

    커뮤니티 페이지

--- src/main/java/com/teamProject/tripPlan/entity/Keyword.java
+++ src/main/java/com/teamProject/tripPlan/entity/Keyword.java
@@ -1,16 +1,24 @@
 package com.teamProject.tripPlan.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-//Getter, Setter || Data 다 오류나길래 일단 모든 entity 다 getter, setter 적었어요..!
+import java.util.ArrayList;
+import java.util.List;
+
+//Getter, Setter || Data 다 오류나길래 일단 모든 entity 다 getter, setter 적었어요!
 @Entity
 public class Keyword {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long keywordId;
     private String keyword;
+
+    @ManyToMany(mappedBy = "keywords", fetch = FetchType.LAZY)
+    private List<Post> posts = new ArrayList<>();
 
     public Keyword() {}
 
@@ -18,12 +26,19 @@ public class Keyword {
         this.keyword = keyword;
     }
 
+    public Long getKeywordId() {
+        return keywordId;
+    }
+
     public String getKeyword() {
         return keyword;
+    }
+
+    public void setKeywordId(Long keywordId) {
+        this.keywordId = keywordId;
     }
 
     public void setKeyword(String keyword) {
         this.keyword = keyword;
     }
-
 }
