599dfc1d374df1e31a134a3de0596c46737bde0c
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-10-29 14:48:32 +0900

    마이페이지

--- src/main/java/com/teamProject/tripPlan/service/MyPageService.java
+++ src/main/java/com/teamProject/tripPlan/service/MyPageService.java
@@ -3,9 +3,7 @@ package com.teamProject.tripPlan.service;
 import com.teamProject.tripPlan.dao.MyPageDAO;
 import com.teamProject.tripPlan.dto.PostDTO;
 import com.teamProject.tripPlan.dto.UsersDTO;
-import com.teamProject.tripPlan.entity.Post;
-import com.teamProject.tripPlan.entity.Travel;
-import com.teamProject.tripPlan.entity.Users;
+import com.teamProject.tripPlan.entity.*;
 import com.teamProject.tripPlan.repository.PostRepository;
 import com.teamProject.tripPlan.repository.UserRepository;
 import jakarta.persistence.EntityManager;
@@ -44,5 +42,15 @@ public class MyPageService {
     public List<Travel> findUserList(Long id) {
         List<Travel> travels = myPageDAO.findUserList(id);
         return travels;
+    }
+
+    public List<Accommodation> findUserAccommodation(Long id) {
+        List<Accommodation> accommodation = myPageDAO.findUserAccommodation(id);
+        return accommodation;
+    }
+
+    public Long findUserId(String userId) {
+        Long id = myPageDAO.findUserId(userId);
+        return id;
     }
 }
