eb264890441cb5b086027ee00d209e032292cda7
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-10-29 14:46:44 +0900

    커뮤니티 페이지

--- src/main/java/com/teamProject/tripPlan/dto/PostDTO.java
+++ src/main/java/com/teamProject/tripPlan/dto/PostDTO.java
@@ -1,5 +1,6 @@
 package com.teamProject.tripPlan.dto;
 
+import com.teamProject.tripPlan.entity.Keyword;
 import com.teamProject.tripPlan.entity.Post;
 import com.teamProject.tripPlan.entity.Travel;
 import com.teamProject.tripPlan.entity.Users;
@@ -26,6 +27,9 @@ public class PostDTO {
     private List<CommentDTO> comments = new ArrayList<>();
     private Travel travel;
 
+    // 추가: keywords 필드
+    private List<Keyword> keywords = new ArrayList<>();
+
     public static PostDTO fromEntity(Post post) {
         return new PostDTO(
                 post.getPostId(),
@@ -34,8 +38,10 @@ public class PostDTO {
                 post.getPostDate(),
                 post.getLikes(),
                 post.getUsers(),
-                post.getComments().stream().map(x->CommentDTO.fromEntity(x)).toList(),
-                post.getTravel());
+                post.getComments().stream().map(CommentDTO::fromEntity).toList(),
+                post.getTravel(),
+                post.getKeywords()  // 키워드 추가
+        );
     }
 
     public static Post fromDTO(PostDTO dto) {
@@ -45,7 +51,7 @@ public class PostDTO {
         post.setPostContent(dto.getPostContent());
         post.setPostDate(dto.getPostDate());
         post.setLikes(dto.getLikes());
+        post.setKeywords(dto.getKeywords()); // 키워드 설정 추가
         return post;
     }
-
 }
