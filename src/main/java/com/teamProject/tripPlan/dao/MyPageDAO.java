af016d637c06815bed077ca495ab33dc8a3add8d
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-10-29 14:45:41 +0900

    마이페이지

--- src/main/java/com/teamProject/tripPlan/dao/MyPageDAO.java
+++ src/main/java/com/teamProject/tripPlan/dao/MyPageDAO.java
@@ -1,9 +1,7 @@
 package com.teamProject.tripPlan.dao;
 
 import com.teamProject.tripPlan.dto.UsersDTO;
-import com.teamProject.tripPlan.entity.Post;
-import com.teamProject.tripPlan.entity.Travel;
-import com.teamProject.tripPlan.entity.Users;
+import com.teamProject.tripPlan.entity.*;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
 import jakarta.transaction.Transactional;
@@ -42,13 +40,35 @@ public class MyPageDAO {
         em.remove(users);
     }
 
+    // 유저에 해당하는 여행 계획 리스트 가져오기 (키워드, 지역, 날짜)
     public List<Travel> findUserList(Long id) {
-        Long id1 = 1L;
         String sql = "SELECT t FROM Travel t " +
                 "JOIN t.users u " +
-                "WHERE u.userNo = " + id1;
+                "WHERE u.userNo = " + id;
         Query query = em.createQuery(sql);
         List<Travel> travels = query.getResultList();
         return travels;
+    }
+
+    // 해당 유저의 여행 계획 리스트 가져오기 (숙소)
+    public List<Accommodation> findUserAccommodation(Long id) {
+        String sql = "SELECT a FROM Accommodation a " +
+                "JOIN TravelAccommodation ta " +
+                "ON a.accommodationId = ta.accommodation.accommodationId " +
+                "JOIN ta.travel t " +
+                "ON ta.travel.travelId = " + id;
+        Query query = em.createQuery(sql);
+        List<Accommodation> accommodation = query.getResultList();
+        return accommodation;
+    }
+
+    public Long findUserId(String userId) {
+        String sql = "SELECT u FROM Users u " +
+                "WHERE u.userId = '" + userId + "'";
+        Query query = em.createQuery(sql);
+        Users users = (Users) query.getSingleResult();
+        Long id = users.getUserNo();
+        System.out.println(id);
+        return id;
     }
 }
