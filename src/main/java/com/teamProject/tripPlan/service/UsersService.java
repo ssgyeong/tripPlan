b0bc89c1d9728b33c74666fafc12e4e1b2c97b6d
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-10-31 23:50:23 +0900

    1031 UsersService.java

--- src/main/java/com/teamProject/tripPlan/service/UsersService.java
+++ src/main/java/com/teamProject/tripPlan/service/UsersService.java
@@ -1,42 +1,43 @@
 package com.teamProject.tripPlan.service;
 
 import com.teamProject.tripPlan.dao.PostDAO;
+import com.teamProject.tripPlan.dao.SuggestionDAO;
+import com.teamProject.tripPlan.dao.UsersDAO;
 import com.teamProject.tripPlan.dto.PostDTO;
-import com.teamProject.tripPlan.dto.UsersDTO;
-import com.teamProject.tripPlan.entity.Post;
-import com.teamProject.tripPlan.entity.Users;
+import com.teamProject.tripPlan.dto.SuggestionDTO;
 import com.teamProject.tripPlan.repository.UserRepository;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.teamProject.tripPlan.dto.UsersDTO;
+import com.teamProject.tripPlan.entity.Users;
 import org.springframework.util.ObjectUtils;
 
 @Service
 @RequiredArgsConstructor
 public class UsersService {
 
-    private final PostDAO postDAO;
-    private final UserRepository userRepository;
+    @Autowired
+    UsersDAO dao;
 
-    public void insertPost(Long userNo, PostDTO dto) {
+    @Autowired
+    UserRepository userRepository;
+
+    private final PostDAO postDAO;
+
+    private final SuggestionDAO suggestionDAO;
+
+    public Long insertPost(Long userNo, PostDTO dto) {
         postDAO.insertPost(userNo, PostDTO.fromDTO(dto));
+        return userNo;
     }
 
+    public void deleteUser(Long userNo) {
+        dao.deleteUser(userNo);
+    }
 
-    public UsersDTO findByMemberId(UsersDTO dto) {
-        Users users = userRepository.findByUserId(dto.getUserId()).orElse(null);
-
-        // 아이디 확인
-        if (ObjectUtils.isEmpty(users)) {
-            return null;
-        }
-        // 비밀번호 확인
-        if (dto.getUserPassword().equals(users.getUserPassword())) {
-            return UsersDTO.fromEntity(users);
-        } else {
-            users.setUserPassword(null);
-            return UsersDTO.fromEntity(users);
-        }
+    public void insertSuggestion(Long userNo, SuggestionDTO dto) {
+        suggestionDAO.insertSuggestion(userNo, SuggestionDTO.fromDTO(dto));
     }
 }
