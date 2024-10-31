51833e25949d810730117cb7b6d6711e7a9db3f2
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-10-31 23:28:21 +0900

    1031 TravelDatesDTO.java

---
+++ src/main/java/com/teamProject/tripPlan/dto/TravelDatesDTO.java
@@ -0,0 +1,11 @@
+package com.teamProject.tripPlan.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TravelDatesDTO {
+    private String startDate;
+    private String endDate;
+}
