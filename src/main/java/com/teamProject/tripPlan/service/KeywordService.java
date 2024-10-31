e238f2191d18129436d46bebbec3d398e4276137
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-10-31 23:45:46 +0900

    1031 KeywordService.java

--- src/main/java/com/teamProject/tripPlan/service/KeywordService.java
+++ src/main/java/com/teamProject/tripPlan/service/KeywordService.java
@@ -2,10 +2,13 @@ package com.teamProject.tripPlan.service;
 
 import com.teamProject.tripPlan.dao.KeywordDAO;
 import com.teamProject.tripPlan.dto.KeywordDTO;
+import com.teamProject.tripPlan.dto.UsersDTO;
 import com.teamProject.tripPlan.entity.Keyword;
+import com.teamProject.tripPlan.entity.Users;
 import com.teamProject.tripPlan.repository.KeywordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import java.util.List;
 
@@ -22,6 +25,12 @@ public class KeywordService {
         return keywordRepository.findAll(); // 모든 키워드 반환
     }
 
+    public KeywordDTO findOneKeyword(Long keywordId) {
+        Keyword keyword = dao.findOneKeyword(keywordId);
+        if (ObjectUtils.isEmpty(keyword)) return null;
+        return KeywordDTO.fromEntity(keyword);
+    }
+
     public void saveKeyword(Keyword keyword) {
         keywordRepository.save(keyword); // 키워드 저장
     }
@@ -32,5 +41,9 @@ public class KeywordService {
 
     public void deleteKeyword(Long keywordId) {
         dao.deleteKeyword(keywordId);
+    }
+
+    public void updateKeyword(KeywordDTO dto) {
+        dao.updateKeyword(dto);
     }
 }
