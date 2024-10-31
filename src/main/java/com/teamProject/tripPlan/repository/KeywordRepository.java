e8112b63689b4bc1187bb8c412dfd749e964875c
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-10-31 23:37:49 +0900

    1031 KeywordRepository.java

--- src/main/java/com/teamProject/tripPlan/repository/KeywordRepository.java
+++ src/main/java/com/teamProject/tripPlan/repository/KeywordRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface KeywordRepository extends JpaRepository<Keyword, String> {
+public interface KeywordRepository extends JpaRepository<Keyword, Long> {
 
 }
