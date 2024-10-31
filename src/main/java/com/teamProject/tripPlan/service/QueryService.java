17876c685e31a9c107fb9fb7179b59c45a52afdd
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-10-31 23:47:21 +0900

    1031 QueryService.java

--- src/main/java/com/teamProject/tripPlan/service/QueryService.java
+++ src/main/java/com/teamProject/tripPlan/service/QueryService.java
@@ -2,6 +2,7 @@ package com.teamProject.tripPlan.service;
 
 import com.teamProject.tripPlan.dto.PostDTO;
 import com.teamProject.tripPlan.entity.Users;
+import com.teamProject.tripPlan.entity.UsersMbtiAnswer;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
@@ -18,12 +19,14 @@ public class QueryService {
     @Autowired
     EntityManager em;
 
+    @Autowired
+    UsersMbtiAnswerService usersMbtiAnswerService;
+
     public Users findOneUser(String userId) {
         String sql = "SELECT u FROM Users u WHERE u.userId = :userId";
 
         TypedQuery<Users> query = em.createQuery(sql, Users.class).setParameter("userId", userId);
         Users users = query.getSingleResult();
-//        Query query = em.createQuery(sql).setParameter("id", id);
         return users;
     }
 }
