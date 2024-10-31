d217c06c481458147e8c94c8bf7285e5e2d1f4e5
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-10-31 23:04:18 +0900

    1031 CustomUserDetails.java

---
+++ src/main/java/com/teamProject/tripPlan/dto/CustomUserDetails.java
@@ -0,0 +1,61 @@
+package com.teamProject.tripPlan.dto;
+
+import com.teamProject.tripPlan.entity.Users;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public class CustomUserDetails implements UserDetails {
+    private Users users;
+
+    public CustomUserDetails(Users users) {
+        this.users = users;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection =
+                new ArrayList<>();
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return users.getRole().toString();
+            }
+        });
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return users.getUserPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return users.getUserId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
