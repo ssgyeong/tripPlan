8f0fd53841043562d3d0089967f1b3482ec1ab76
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-10-31 23:00:31 +0900

    1031 CommentDAO.java

--- src/main/java/com/teamProject/tripPlan/dao/CommentDAO.java
+++ src/main/java/com/teamProject/tripPlan/dao/CommentDAO.java
@@ -2,6 +2,7 @@ package com.teamProject.tripPlan.dao;
 
 import com.teamProject.tripPlan.entity.Comment;
 import com.teamProject.tripPlan.entity.Post;
+import com.teamProject.tripPlan.entity.Suggestion;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,15 +25,30 @@ public class CommentDAO {
         em.persist(post);
     }
 
+    public void insertSuggestionComment(Long suggestionId, Comment comment) {
+        Suggestion suggestion = em.find(Suggestion.class, suggestionId);
+        comment.setSuggestion(suggestion);
+        comment.setCommentNickname("admin"); // 현재 로그인되어있는 사람의 닉네임으로 변경. (session.logId의 아이디)
+        suggestion.getComments().add(comment);
+        em.persist(suggestion);
+    }
+
     public Long deleteComment(Long commentId) {
         Comment comment = em.find(Comment.class, commentId);
         em.remove(comment);
         return comment.getPost().getPostId();
     }
 
+    public Long deleteSuggestionComment(Long commentId) {
+        Comment comment = em.find(Comment.class, commentId);
+        em.remove(comment);
+        return comment.getSuggestion().getSuggestionId();
+    }
+
     public Comment findByCommentId(Long commentId) {
         return em.find(Comment.class, commentId);
     }
+
 
     public void updateComment(Comment comment) {
         Comment newComment = em.find(Comment.class, comment.getCommentId());
