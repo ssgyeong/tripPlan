7fe3b9fa700f1fbdf7dc4fcde3366817cb955de3
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-10-31 23:35:53 +0900

    1031 Users.java

--- src/main/java/com/teamProject/tripPlan/entity/Users.java
+++ src/main/java/com/teamProject/tripPlan/entity/Users.java
@@ -3,6 +3,7 @@ package com.teamProject.tripPlan.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +11,7 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
-
+@ToString
 public class Users {
 
     @Id
@@ -18,16 +19,32 @@ public class Users {
     private Long userNo;
     @Column(nullable = false, length = 30)
     private String userId;
-    @Column(nullable = false, length = 30)
+    @Column(nullable = false, length = 255)
     private String userPassword;
-    @Column(nullable = false, length = 20)
+    @Column(nullable = false, length = 30)
     private String userName;
     private String userNickname;
     @Column(nullable = false, length = 30)
     private String userEmail;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
 
     @OneToMany(fetch = FetchType.LAZY,
             mappedBy = "users",
             cascade = CascadeType.PERSIST)
-    List<Post> posts = new ArrayList<>();
+    private List<Post> posts = new ArrayList<>();
+
+    @OneToMany(fetch = FetchType.LAZY,
+            mappedBy = "users",
+            cascade = CascadeType.PERSIST)
+    private List<Suggestion> suggestions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "users",
+            cascade = {CascadeType.PERSIST, CascadeType.REMOVE},
+            fetch = FetchType.LAZY)
+    private List<UsersMbtiAnswer> usersMbtiAnswer = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    private ResultType resultType;
+
 }
