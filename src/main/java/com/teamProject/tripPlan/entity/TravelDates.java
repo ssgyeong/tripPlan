c3f13b9b5542888b40a81793858c36e7cf8cc6ec
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-10-31 23:35:08 +0900

    1031 TravelDates.java

--- src/main/java/com/teamProject/tripPlan/entity/TravelDates.java
+++ src/main/java/com/teamProject/tripPlan/entity/TravelDates.java
@@ -1,46 +1,19 @@
 package com.teamProject.tripPlan.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 import java.sql.Date;
 
+@Data
 @Entity
 public class TravelDates {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int travelId;
     private Date departure_date;
     private Date arrival_date;
-
-    public TravelDates() {}
-
-    public TravelDates(int travelId, Date departureDate, Date arrivalDate) {
-        this.travelId = travelId;
-        this.departure_date = departureDate;
-        this.arrival_date = arrivalDate;
-    }
-
-    public int getTravelId() {
-        return travelId;
-    }
-
-    public void setTravelId(int travelId) {
-        this.travelId = travelId;
-    }
-
-    public Date getDepartureDate() {
-        return departure_date;
-    }
-
-    public void setDepartureDate(Date departureDate) {
-        this.departure_date = departureDate;
-    }
-
-    public Date getArrivalDate() {
-        return arrival_date;
-    }
-
-    public void setArrivalDate(Date arrivalDate) {
-        this.arrival_date = arrivalDate;
-    }
 }
