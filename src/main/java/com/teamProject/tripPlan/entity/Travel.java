7ceff84046104a914734a8fccee7927adbd692fc
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-10-31 23:34:29 +0900

    1031 Travel.java

--- src/main/java/com/teamProject/tripPlan/entity/Travel.java
+++ src/main/java/com/teamProject/tripPlan/entity/Travel.java
@@ -1,8 +1,7 @@
 package com.teamProject.tripPlan.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -16,6 +15,7 @@ public class Travel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long travelId;
+
     @Column(length = 20)
     private String keyword;
     private String place;
@@ -23,25 +23,17 @@ public class Travel {
     private LocalDateTime endDate;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_no")
     private Users users;
 
-    @OneToMany(fetch = FetchType.LAZY,
-            mappedBy = "travel",
-            cascade = CascadeType.PERSIST)
-    List<TravelRestaurant> travelRestaurants = new ArrayList<>();
-
-    @OneToMany(fetch = FetchType.LAZY,
-            mappedBy = "travel",
-            cascade = CascadeType.PERSIST)
-    List<TravelAttraction> travelAttractions = new ArrayList<>();
-
-    @OneToMany(fetch = FetchType.LAZY,
-            mappedBy = "travel",
-            cascade = CascadeType.PERSIST)
-    List<TravelAccommodation> travelAccommodations = new ArrayList<>();
+    private String accommodation; // 이용한 숙소
+    private String restaurant; // 이용한 식당
+    private String attractions; // 관광지
 
     @OneToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "travel_id")
     private Post post;
+
+    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "travel", fetch = FetchType.LAZY)
+    private List<MyList> myLists = new ArrayList<>();
 }
