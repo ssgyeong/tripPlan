c9516b7413491b441832604d026bd15d37e5c767
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-10-31 23:38:08 +0900

    1031 MbtiTestResultRepository.java

---
+++ src/main/java/com/teamProject/tripPlan/repository/MbtiTestResultRepository.java
@@ -0,0 +1,7 @@
+package com.teamProject.tripPlan.repository;
+
+import com.teamProject.tripPlan.entity.MbtiTestResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MbtiTestResultRepository extends JpaRepository<MbtiTestResult, Long> {
+}
