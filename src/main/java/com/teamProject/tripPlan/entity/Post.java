18ab1b0044a832d25f2b5e62080546de1029b373
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-10-31 23:32:57 +0900

    1031 Post.java

--- src/main/java/com/teamProject/tripPlan/entity/Post.java
+++ src/main/java/com/teamProject/tripPlan/entity/Post.java
@@ -1,8 +1,10 @@
 package com.teamProject.tripPlan.entity;
 
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -12,7 +14,9 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
-public class Post {
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class Post extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long postId;
@@ -20,7 +24,6 @@ public class Post {
     private String postTitle;
     @Column(length = 1000)
     private String postContent;
-    private LocalDateTime postDate;
     private int likes;
 
     public Post() {
@@ -28,21 +31,17 @@ public class Post {
     }
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "post",
-            cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+            cascade = {CascadeType.PERSIST, CascadeType.ALL})
     private List<Comment> comments = new ArrayList<>();
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_no")
     private Users users;
 
-    @OneToOne(mappedBy = "post", fetch = FetchType.LAZY)
+    @OneToOne(mappedBy = "post", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private Travel travel;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
-    @JoinTable(
-            name = "post_keyword",
-            joinColumns = @JoinColumn(name = "post_id"),
-            inverseJoinColumns = @JoinColumn(name = "keyword_id")
-    )
-    private List<Keyword> keywords = new ArrayList<>();
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "keyword_id")
+    private Keyword keyword;
 }
