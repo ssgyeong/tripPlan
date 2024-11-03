e5bd653b53e14ad4f21954fe595506059d05f414
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-11-03 17:09:52 +0900

    1103 JoinRepository.java

--- src/main/java/com/teamProject/tripPlan/repository/JoinRepository.java
+++ src/main/java/com/teamProject/tripPlan/repository/JoinRepository.java
@@ -5,7 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JoinRepository extends JpaRepository<Users, String> {
     // 기존 아이디와 비교
-    Boolean existsByUserId(String userId);
+    boolean existsByUserId(String userId);
+
+    // 기존 닉네임과 비교
+    boolean existsByUserNickname(String userNickname);
+
+    // 기존 이메일과 비교
+    boolean existsByUserEmail(String userEmail);
 
     // userId로 사용자 찾기
     Users findByUserId(String userId);
