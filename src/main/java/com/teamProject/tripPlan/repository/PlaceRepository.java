88450175450da5b966c05dab3d36ca041a4b4465
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-11-03 17:10:47 +0900

    1103 PlaceRepository.java

---
+++ src/main/java/com/teamProject/tripPlan/repository/PlaceRepository.java
@@ -0,0 +1,7 @@
+package com.teamProject.tripPlan.repository;
+
+import com.teamProject.tripPlan.entity.Place;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaceRepository extends JpaRepository<Place, Long> {
+}
