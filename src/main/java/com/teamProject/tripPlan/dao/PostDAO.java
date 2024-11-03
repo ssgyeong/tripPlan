6a67b37ed97d28a3fb994da0d22c6fe6b2329f11
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-11-03 16:55:33 +0900

    1103 PostDAO.java

--- src/main/java/com/teamProject/tripPlan/dao/PostDAO.java
+++ src/main/java/com/teamProject/tripPlan/dao/PostDAO.java
@@ -4,11 +4,13 @@ import com.teamProject.tripPlan.dto.PostDTO;
 import com.teamProject.tripPlan.entity.Post;
 import com.teamProject.tripPlan.entity.Users;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.TypedQuery;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
 
 @Component
@@ -26,7 +28,11 @@ public class PostDAO {
     }
 
     public List<Post> findAllPost() {
-        return em.createQuery("SELECT p FROM Post p LEFT JOIN FETCH p.keyword", Post.class).getResultList();
+        String sql = "SELECT p FROM Post p " +
+                "LEFT JOIN FETCH p.keyword " +
+                "ORDER BY p.postId DESC";
+
+        return em.createQuery(sql, Post.class).getResultList();
     }
 
     public Post getOnePost(Long postId) {
@@ -35,26 +41,32 @@ public class PostDAO {
     }
 
     public void deleteById(Long postId) {
-        Post post = em.find(Post.class, postId);
-        em.remove(post);
+        String sql = "DELETE FROM Post p WHERE p.postId = :postId";
+        em.createQuery(sql).setParameter("postId", postId).executeUpdate();
     }
 
     public void updatePost(PostDTO dto) {
         Post post = em.find(Post.class, dto.getPostId());
         post.setPostTitle(dto.getPostTitle());
         post.setPostContent(dto.getPostContent());
+        post.setKeyword(dto.getKeyword());
     }
 
     public void calculateLikes(Long id) {
         Post post = getOnePost(id);
-        post.setLikes(post.getLikes()+1);
-        em.persist(post);
+        if (post != null) {  // 게시글이 존재하는 경우에만 처리
+            post.setLikes(post.getLikes() + 1);
+            em.persist(post);  // 변경 사항을 저장
+            System.out.println("Likes incremented for post with ID: " + id);
+        } else {
+            System.out.println("Post with ID " + id + " not found. Cannot increment likes.");
+        }
     }
 
-    public List<Post> findPostsByKeywords(String keyword) {
-        String jpql = "SELECT DISTINCT p FROM Post p JOIN p.keyword k WHERE k.keyword IN :keyword";
+    public List<Post> findPostsByKeywordId(Long keywordId) {
+        String jpql = "SELECT p FROM Post p WHERE p.keyword.keywordId = :keywordId";
         return em.createQuery(jpql, Post.class)
-                .setParameter("keyword", keyword)
+                .setParameter("keywordId", keywordId)
                 .getResultList();
     }
 }
