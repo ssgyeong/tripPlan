2a2005906d3d327efc5c954d73da4c7e3bb8cbfd
ssgyeong <148539847+ssgyeong@users.noreply.github.com>
2024-11-03 17:07:57 +0900

    1103 Travel.java

--- src/main/java/com/teamProject/tripPlan/entity/Travel.java
+++ src/main/java/com/teamProject/tripPlan/entity/Travel.java
@@ -16,24 +16,14 @@ public class Travel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long travelId;
 
-    @Column(length = 20)
-    private String keyword;
-    private String place;
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_no")
     private Users users;
-
-    private String accommodation; // 이용한 숙소
-    private String restaurant; // 이용한 식당
-    private String attractions; // 관광지
 
     @OneToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "travel_id")
     private Post post;
 
     @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "travel", fetch = FetchType.LAZY)
-    private List<MyList> myLists = new ArrayList<>();
+    private List<Place> places = new ArrayList<>();
 }
